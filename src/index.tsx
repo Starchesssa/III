7255cd4eef70ef8ca94ba0eab5513cd4bc21c6d5
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-09-08 10:55:54 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
+import {registerRoot} from 'remotion';
+import {Video} from './Video';
 
-import { registerRoot } from 'remotion';
-import { RemotionVideo } from './Video'; // your main video component
-
-// Register the main video component as the root
-registerRoot(RemotionVideo);
+registerRoot(Video);
