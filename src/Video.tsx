a7c9e540ebe7f3262f4cbe14fa8a4237b8b002b4
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-09-08 10:59:04 +0300

    Update Video.tsx

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,22 +1,61 @@
-// src/Video.tsx
-import {Composition} from 'remotion';
-import {AmazonPhilosophy} from './AmazonPhilosophy';
 
-// Audio duration is ~69 seconds. Let's make the video 70 seconds long at 30 FPS.
-const VIDEO_DURATION_IN_SECONDS = 70;
+
+import {Composition, Series, Audio} from 'remotion';
+import {Scene1} from './scenes/Scene1';
+import {Scene2} from './scenes/Scene2';
+import {Scene3} from './scenes/Scene3';
+import {Scene4} from './scenes/Scene4';
+import {Scene5} from './scenes/Scene5';
+import {Scene6} from './scenes/Scene6';
+import {Scene7} from './scenes/Scene7';
+
+import audio from '../public/audio.mp3';
+
 const FPS = 30;
+const DURATION_IN_SECONDS = 69;
 
-export const RemotionVideo: React.FC = () => {
+export const Video = () => {
 	return (
 		<>
 			<Composition
-				id="Amazon-Failure-To-Success"
-				component={AmazonPhilosophy}
-				durationInFrames={VIDEO_DURATION_IN_SECONDS * FPS}
+				id="AmazonFailure"
+				component={Main}
+				durationInFrames={DURATION_IN_SECONDS * FPS}
 				fps={FPS}
 				width={1080}
-				height={1920} // Vertical format for social media
+				height={1920}
 			/>
 		</>
+	);
+};
+
+const Main = () => {
+	return (
+		<div style={{backgroundColor: '#111827'}}>
+			<Series>
+				<Series.Sequence durationInFrames={9 * FPS}>
+					<Scene1 />
+				</Series.Sequence>
+				<Series.Sequence durationInFrames={6 * FPS}>
+					<Scene2 />
+				</Series.Sequence>
+				<Series.Sequence durationInFrames={8 * FPS}>
+					<Scene3 />
+				</Series.Sequence>
+				<Series.Sequence durationInFrames={8 * FPS}>
+					<Scene4 />
+				</Series.Sequence>
+				<Series.Sequence durationInFrames={9 * FPS}>
+					<Scene5 />
+				</Series.Sequence>
+				<Series.Sequence durationInFrames={11 * FPS}>
+					<Scene6 />
+				</Series.Sequence>
+				<Series.Sequence durationInFrames={18 * FPS}>
+					<Scene7 />
+				</Series.Sequence>
+			</Series>
+			<Audio src={audio} />
+		</div>
 	);
 };
