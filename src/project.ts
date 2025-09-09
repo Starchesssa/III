c558e30cc9466d382da8b87e4d8305ceabb4a510
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-09-09 15:04:48 +0300

    Update project.ts

--- src/project.ts
+++ src/project.ts
@@ -1,21 +1,11 @@
 
+// src/project.ts
 import {makeProject} from '@motion-canvas/core';
-import {FFmpeg} from '@motion-canvas/ffmpeg';
 
-import main from './scenes/main?scene';
-import audio from '../public/audio.wav';
+import main from './main.tsx';
+import './global.css';
 
-// Project configuration for CLI rendering
 export default makeProject({
   scenes: [main],
-  audio: audio,
-  output: {
-    // Using FFmpeg for headless rendering
-    renderer: new FFmpeg({
-      crf: 18, // High-quality video setting
-      preset: 'slow',
-    }),
-    // Standard 1080p video resolution
-    size: {width: 1920, height: 1080},
-  },
+  audio: './public/audio.wav',
 });
