64bf7eddd7158745e64d4c207d36f90b0c54194b
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2025-09-10 07:55:15 +0300

    Update Video.tsx

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,61 +1,24 @@
+TypeScript
+import { Composition } from 'remotion';
+import { MainScene } from './scenes/MainScene';
+import './style.css'; // We'll create this for fonts and basic styles
 
+// Audio duration is approximately 70 seconds. Let's set the duration a bit longer.
+const VIDEO_DURATION_IN_FRAMES = 70 * 30; // 2100 frames
+const VIDEO_WIDTH = 1080;
+const VIDEO_HEIGHT = 1920;
 
-import {Composition, Series, Audio} from 'remotion';
-import {Scene1} from './scenes/Scene1';
-import {Scene2} from './scenes/Scene2';
-import {Scene3} from './scenes/Scene3';
-import {Scene4} from './scenes/Scene4';
-import {Scene5} from './scenes/Scene5';
-import {Scene6} from './scenes/Scene6';
-import {Scene7} from './scenes/Scene7';
-
-import audio from '../public/audio.wav';
-
-const FPS = 30;
-const DURATION_IN_SECONDS = 69;
-
-export const Video = () => {
+export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<Composition
 				id="AmazonFailure"
-				component={Main}
-				durationInFrames={DURATION_IN_SECONDS * FPS}
-				fps={FPS}
-				width={1080}
-				height={1920}
+				component={MainScene}
+				durationInFrames={VIDEO_DURATION_IN_FRAMES}
+				fps={30}
+				width={VIDEO_WIDTH}
+				height={VIDEO_HEIGHT}
 			/>
 		</>
-	);
-};
-
-const Main = () => {
-	return (
-		<div style={{backgroundColor: '#111827'}}>
-			<Series>
-				<Series.Sequence durationInFrames={9 * FPS}>
-					<Scene1 />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={6 * FPS}>
-					<Scene2 />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={8 * FPS}>
-					<Scene3 />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={8 * FPS}>
-					<Scene4 />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={9 * FPS}>
-					<Scene5 />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={11 * FPS}>
-					<Scene6 />
-				</Series.Sequence>
-				<Series.Sequence durationInFrames={18 * FPS}>
-					<Scene7 />
-				</Series.Sequence>
-			</Series>
-			<Audio src={audio} />
-		</div>
 	);
 };
